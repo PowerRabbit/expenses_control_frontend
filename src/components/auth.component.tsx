72616592d9a6918fe6ca54cc96ab8e2642de868e
Ivan Vlasov <ivan.vlassov@gmail.com>
2019-10-21 20:53:14 +0200

    Add simple auth form

--- src/components/auth.component.tsx
+++ src/components/auth.component.tsx
@@ -1,15 +1,39 @@
-import React, { Component } from 'react';
+import React, { FormEvent, useState } from 'react';
 
-const SERVER_NAME = 'http://localhost:5000/';
-const URL = 'expenses-control/us-central1/logIn';
+import { EcAPI } from '../services/api.service';
+import { EcError } from '../services/error.service';
 
-export class EcAuth extends Component {
 
-  render() {
+//SETTINGS.SERVER_URL
+
+export function EcAuth() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function sendCredentials(e: FormEvent<HTMLFormElement>) {
+        (e as unknown as Event).stopPropagation();
+        (e as unknown as Event).preventDefault();
+
+        //EcAPI.logIn()
+        if (username && password) {
+            EcAPI.logIn(username, password).then((response) => {
+                console.log(response);
+            });
+        } else {
+            EcError.showError('Please enter username and password!');
+        }
+    }
+
     return (
-      <div>
-        Auth
-      </div>
+        <div>
+
+            <form onSubmit={sendCredentials}>
+                <input type="text" placeholder="Username" value={username} onChange={e => setUsername((e.target as any).value)} />
+                <input type="password" placeholder="Password" value={password} onChange={e => setPassword((e.target as any).value)} />
+                <button type="submit">Log In</button>
+            </form>
+
+        </div>
     );
-  }
 }
---
+++ src/services/error.service.ts
@@ -0,0 +1,7 @@
+export const EcError = {  // no namespace support at the moment
+
+    showError: (message: string) => {
+        alert(message || 'An error has occured.');
+    }
+
+}
